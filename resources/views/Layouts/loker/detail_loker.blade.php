f09b3f4bcec1a9363ee7162e88c0af3ff28cdb21
Felisa Rahmawati <felisarahmawati6@gmail.com>
2022-12-09 22:01:16 +0700

    login pekerja eror

--- resources/views/Layouts/loker/detail_loker.blade.php
+++ resources/views/Layouts/loker/detail_loker.blade.php
@@ -24,49 +24,123 @@
                 </div>
             </div>
         </div>
-        <div class=" box2 card px-2">
-            <div class=" col-md-12 ml-auto">
-                <div class="heading_container remove_line_bt">
-                    <h2>
-                        Deskripsi Lowongan
-                    </h2>
-                    <p>
-                       PT.BRAXY sedang membutuhkan posisi lowongan kerja sebagai berikut 
-                    </p>
-                    <ul style="margin-top: 20px;">
-                        <b>
-                        <li>Kasir</li>
-                        <li>Cook helper</li>
-                        </b>
-                    </ul>
-                    <p style="margin-top: 20px;">
-                        - Pria/wanitam Maks 27 tahun<br>
-                        - Pendidikan Min. SMP<br>
-                        - Mampu bekerja Tim<br>
-                        - Dapat berkomunikasi dengan baik
-                    </p>
-                    <p style="margin-top: 20px;">
-                        <b>Kirim lamaran & CV ke:</b><br>
-                        ptbraxy@gmail.com
-                    </p>
+        <br>
+        <div class="container">
+            <div class="single-job">
+                <div class="job-tab">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-3 col-xs-12">
+                            <div class="post-media">
+                                <img class="img-thumbnail" src="<img width="150" height="150" src="https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?fit=150%2C150&amp;ssl=1" class="img-responsive wp-post-image" alt="" loading="lazy" srcset="https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?w=500&amp;ssl=1 500w, https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?resize=300%2C300&amp;ssl=1 300w, https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?resize=150%2C150&amp;ssl=1 150w" sizes="(max-width: 150px) 100vw, 150px" data-attachment-id="56950" data-permalink="https://www.jogjalowker.co.id/?attachment_id=56950" data-orig-file="https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?fit=500%2C500&amp;ssl=1" data-orig-size="500,500" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="jogja java batik" data-image-description="" data-image-caption="" data-medium-file="https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?fit=300%2C300&amp;ssl=1" data-large-file="https://i0.wp.com/www.jogjalowker.co.id/wp-content/uploads/2020/10/jogja-java-batik.png?fit=500%2C500&amp;ssl=1" />
+                            </div>
+                            <li class="badge freelancer-badge" favorit" itemprop="employmentType">Favorite</li>
+                            <li class="badge freelancer-badge" full-time" itemprop="employmentType">Full Time</li>
+                            <h3>
+                                <a title="">
+                                    Admin Order  - Content Creator - Model Reviewer
+                                </a>
+                            </h3>
+                            <small>
+                                <span><img src="https://i0.wp.com/www.jogjalowker.co.id/wp-content/themes/Web/images/icon1.png?ssl=1" alt="" data-recalc-dims="1"> : <a >Java in Batik</a></span><br>
+                                <span><img src="https://i0.wp.com/www.jogjalowker.co.id/wp-content/themes/Web/images/icon2.png?ssl=1" alt=""  data-recalc-dims="1"> : <a>SMA</a></span><br>
+                                <span> <img src="https://i0.wp.com/www.jogjalowker.co.id/wp-content/themes/Web/images/i4.png?ssl=1" alt="" data-recalc-dims="1"> : <a <li>Gaji Kompetitif</li></a></a></span><br>
+                                <span><img src="https://i0.wp.com/www.jogjalowker.co.id/wp-content/themes/Web/images/i3.png?ssl=1" alt="" data-recalc-dims="1"> : <time datetime="2022-12-06">Posted 5 hours ago</time></span>
+                            </small>
+                            <hr>
+                        </div>
 
-                    <p style="margin-top: 20px;">
-                        <b>Atau ke alamat kami :</b><br>
-                        JL.Umbulharjo No.342 
-                    </p>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <div class="content-title">
+                                <h4>Deskripsi Lowongan</h4>
+                                <hr>
+                            </div>
+                            <p class="lead"  > <p><strong>Java in Batik</strong> Sedang membutuhkan kandidat untuk mengisi posisi lowongan kerja di jogja sebagai :</p>
+                                <p>
+                                    <strong>- Admin Order</strong><br/>
+                                    <strong>- Content Creator </strong><br/>
+                                    <strong>- Model Reviewer</strong>
+                                </p>
 
-                    <p style="margin-top: 20px;">
-                        <b>Info lebih lanjut</b><br>
-                        0898785263546
-                    </p>
+                                <p>
+                                    <strong>Admin Order</strong><br/>
+                                    - Wanita single max 28th & Pend Min Sl<br/>
+                                    - Menguasai SOSMED dan Marketplace<br/>
+                                    - Siap HANDLE orderan di luar jam kerja<br/>
+                                    - Suka berjualan lebih di utamakan<br/>
+                                    - Amanah , telaten , tidak baperan dan bertanggungjawab atas pekerjaan<br/>
+                                    - Bisa bekerja dibawah tekanan dan memenuhi target<br/>
+                                    - Mempunyai laptop sendiri<br/>
+                                    - Domisili JOGJA , kontak kerja MIN 1 Tahun
+                                </p>
+
+                                <p>
+                                    <strong>Content Creator</strong><br/>
+                                    - Pria / Wanita single max 28th Pend Min D3<br/>
+                                    - Aktif dan mengikuti update trend di tiktok , instagram dan sosmed lainnya<br/>
+                                    - Menguasai pengambilan foto dan video menggunakan camera dan HP<br/>
+                                    - Menguasai software editing menggunakan HP atau Laptop sesuai kebutuhan konten<br/>
+                                    - Mempunyai laptop sendiri<br/>
+                                    - Niat kerja dan siap bekerja dibawah tekanan<br/>
+                                    - Domisili JOGJA , kontrak MIN 1 tahun<br/>
+                                    - Cantumkan portofolio
+                                </p>
+
+                                <p>
+                                    <strong>
+                                        Model Reviewer
+                                    </strong><br/>
+                                    - Wanita muslim single max 27th<br/>
+                                    - Memakai hijab untuk keseharian<br/>
+                                    - Good looking , percaya diri dan komunikatif<br/>
+                                    - Terbiasa pose di depan camera<br/>
+                                    - Niat kerja dan mempunyai attitude yg baik<br/>
+                                    - Tinggi badan min 158cm , BB proposional<br/>
+                                    - Domisili JOGJA , kontrak MIN 1 tahun<br/>
+                                    - Cantumkan portofolio
+                                </p>
+
+                                <p>Kirim lamaran & CV ke :<br/>
+                                    <strong>
+                                        javainbatik@gmail.com
+                                    </strong>
+                                </p>
+
+                                <p>
+                                    <strong>
+                                        Subject :
+                                    </strong>
+                                    Posisi yang dilamar
+                                </p>
+
+                                <p>
+                                    <strong>
+                                        Java in Batik
+                                    </strong><br/>
+                                    Jl. Manggis no 57A Gaten Depok Sleman Yogyakarta (Timur Kampung Wisata Kuline Pringwulung)
+                                </p>
+
+                                <p>
+                                    Info lebih lanjut :<br/>
+                                    <strong>
+                                        WA : 0877-3805-6630
+                                    </strong>
+                                </p>
+                            </p>
+                        </div>
+
+                        <center>
+                            <div class="col.md-6" style="align-content: center;">
+                                <button type="button" class="btn btn-info text-white" data-bs-toggle="modal" data-bs-target="#DetailUser">
+                                    Lamar Sekarang
+                                </button>
+                            </div>
+                        </center>
+                    </div>
                 </div>
-                {{-- linknya di hubungkan ke email atau whatsapp --}}
-                <a href="" class="btn btn-info col-sm-3" style="margin-top:30px; "> Lamar kerja
-                   
-                </a>
             </div>
         </div>
     </div>
-   </section> 
+
+   </section>
    @include('Layouts.home.footer')
 </section>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-7">
             <div class="card">
-                <div class="card-header">{{ __('LOGIN') }}</div>
+                <div class="card-header" style="text-align: center;">LOGIN</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
